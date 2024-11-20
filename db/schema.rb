98ff240867be48c63b1fd5c93ca203c427bb6aab
Artur Inoue <arturcorreainoue@gmail.com>
2024-11-20 15:48:31 +0000

    made things i dont remebmber what i did exactly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_20_121247) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_20_145407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_20_121247) do
     t.bigint "shoe_id", null: false
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "pending"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
